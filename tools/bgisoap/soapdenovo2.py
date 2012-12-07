b4e5be57bc4c99f59b30e178a26a745045214335
pli888 <peter@gigasciencejournal.com>
2012-12-07 10:23:39 +0800

    Added use of soapdenovo 127mer

--- tools/bgisoap/soapdenovo2.py
+++ tools/bgisoap/soapdenovo2.py
@@ -1,8 +1,8 @@
 """
-soapdenovo.py
-A wrapper script for SOAPdenovo-trans
-Copyright   Peter Li - GigaScience and BGI-HK
-            Huayan Gao - CUHK
+soapdenovo2.py
+A wrapper script for SOAPdenovo2
+Peter Li - GigaScience and BGI-HK
+Huayan Gao - CUHK
 """
 
 import optparse, os, shutil, subprocess, sys, tempfile
@@ -52,17 +52,16 @@ def main():
     #Custom params
     parser.add_option("-K", "--kmer_size", dest="kmer_size", help="kmer size")
     parser.add_option("-p", "--ncpu", dest="ncpu", help="Number of cpu for use")
-    parser.add_option("-a", "--init_mem_assumption", dest="init_mem_assumption", help="<emory assumption initialized to avoid further reallocation, unit G")
+    parser.add_option("-a", "--init_mem_assumption", dest="init_mem_assumption", help="Memory assumption initialized to avoid further reallocation, unit G")
     parser.add_option("-d", "--kmer_freq_cutoff", dest="kmer_freq_cutoff", help="kmers with frequency no larger than KmerFreqCutoff will be deleted")
     parser.add_option("-R", "--resolve_repeats", dest="resolve_repeats", help="Resolve repeats by reads")
     parser.add_option("-D", "--edge_cov_cutoff", dest="edge_cov_cutoff", help="Edges with coverage no larger than EdgeCovCutoff will be deleted")
     parser.add_option("-M", "--merge_level", dest="merge_level", help="The strength of merging similar sequences during contiging")
     parser.add_option("-m", "--max_k", dest="max_k", help="max k when using multi kmer")
     parser.add_option("-e", "--weight", dest="weight", help="weight to filter arc when linearize two edges")
-    parser.add_option("-r", "--keep_avail_read", dest="keep_avail_read", help="keep available read")
+    #parser.add_option("-r", "--keep_avail_read", dest="keep_avail_read", help="keep available read")
     parser.add_option("-E", "--merge_clean_bubble", dest="merge_clean_bubble", help="Merge clean bubble before iterate")
-    parser.add_option("-f", "--output_gap_related_reads", dest="output_gap_related_reads",
-        help="Output gap related reads in map step for using SRkgf to fill gap")
+    #parser.add_option("-f", "--output_gap_related_reads", dest="output_gap_related_reads", help="Output gap related reads in map step for using SRkgf to fill gap")
     parser.add_option("-k", "--kmer_r2c", dest="kmer_r2c", help="kmer size used for mapping read to contig")
     parser.add_option("-F", "--fill_gaps", dest="fill_gaps", help="Fill gaps in scaffold")
     parser.add_option("-u", "--unmask_contigs", dest="unmask_contigs", help="Unmask contigs with high/low coverage before scaffolding")
@@ -84,68 +83,78 @@ def main():
 
     #Create temp directory for performing analysis
     tmp_dir = tempfile.mkdtemp()
-    #Create temp file for configuration
-    tmp_file = tempfile.NamedTemporaryFile(dir=tmp_dir).name
-    try:
-        fout = open(tmp_file,'wb')
-        fout.write("max_rd_len=%s\n" % opts.max_read_length)
-        #Calculate how sets of data there are - use avg_ins as a measure of this
-        #Loop thru this number of times
-        #Also use separate index to keep count of reads
-        single_read_index = 0
-        paired_read_index = 0
-        for index in range(len(opts.avg_insert_list)):
-            print "single_read_index ", single_read_index
-            print "paired_read_index ", paired_read_index
-            fout.write("[LIB]\n")
-            fout.write("avg_ins=%s\n" % (opts.avg_insert_list)[index])
-            fout.write("reverse_seq=%s\n" % opts.reverse_seq_list[index])
-            fout.write("asm_flags=%s\n" % opts.asm_flags_list[index])
-            fout.write("rd_len_cutoff=%s\n" % opts.rd_len_cutoff_list[index])
-            fout.write("rank=%s\n" % opts.rank_list[index])
-            fout.write("pair_num_cutoff=%s\n" % opts.pair_num_cutoff_list[index])
-            fout.write("map_len=%s\n" % opts.map_len_list[index])
-            #Add data file configuration - needs careful looping due to single and paired reads
-            print opts.type_of_data_list[index]
-            print opts.format_of_data_list[index]
-            if opts.type_of_data_list[index] == "single":  #then only one read
-                if (opts.format_of_data_list)[index] == "fastq":
-                    fout.write("q=%s\n" % (opts.single_fastq_input1_list)[single_read_index])
-                elif opts.format_of_data == "fasta":
-                    fout.write("f=%s\n" % opts.single_fasta_input1_list[single_read_index])
-                else:
-                    fout.write("b=%s\n" % opts.single_bam_input1_list[single_read_index])
-                single_read_index = single_read_index + 1
-            elif opts.type_of_data_list[index] == "paired":
-                if opts.format_of_data_list[index] == "fastq":
-                    fout.write("q1=%s\n" % (opts.paired_fastq_input1_list)[paired_read_index])
-                    fout.write("q2=%s\n" % (opts.paired_fastq_input2_list)[paired_read_index])
-                elif opts.format_of_data_list[index] == "fasta":
-                    fout.write("f1=%s\n" % opts.paired_fasta_input1_list[paired_read_index])
-                    fout.write("f2=%s\n" % opts.paired_fasta_input2_list[paired_read_index])
-                else:
-                    fout.write("b1=%s\n" % opts.paired_fasta_input1_list[paired_read_index])
-                    fout.write("b2=%s\n" % opts.paired_fasta_input2_list[paired_read_index])
-                paired_read_index = paired_read_index + 1
-        fout.close()
-    except Exception, e:
-        stop_err("Output config file cannot be opened for writing." + str(e))
 
+    if opts.file_source == "history":
+        config_file = opts.config
+    else:
+        #Create temp file for configuration
+        config_file = tempfile.NamedTemporaryFile(dir=tmp_dir).name
+        try:
+            fout = open(config_file,'wb')
+            fout.write("max_rd_len=%s\n" % opts.max_read_length)
+            #Calculate how sets of data there are - use avg_ins as a measure of this
+            #Loop thru this number of times
+            #Also use separate index to keep count of reads
+            single_read_index = 0
+            paired_read_index = 0
+            for index in range(len(opts.avg_insert_list)):
+                print "single_read_index ", single_read_index
+                print "paired_read_index ", paired_read_index
+                fout.write("[LIB]\n")
+                fout.write("avg_ins=%s\n" % (opts.avg_insert_list)[index])
+                fout.write("reverse_seq=%s\n" % opts.reverse_seq_list[index])
+                fout.write("asm_flags=%s\n" % opts.asm_flags_list[index])
+                fout.write("rd_len_cutoff=%s\n" % opts.rd_len_cutoff_list[index])
+                fout.write("rank=%s\n" % opts.rank_list[index])
+                fout.write("pair_num_cutoff=%s\n" % opts.pair_num_cutoff_list[index])
+                fout.write("map_len=%s\n" % opts.map_len_list[index])
+                #Add data file configuration - needs careful looping due to single and paired reads
+                print opts.type_of_data_list[index]
+                print opts.format_of_data_list[index]
+                if opts.type_of_data_list[index] == "single":  #then only one read
+                    if (opts.format_of_data_list)[index] == "fastq":
+                        fout.write("q=%s\n" % (opts.single_fastq_input1_list)[single_read_index])
+                    elif opts.format_of_data == "fasta":
+                        fout.write("f=%s\n" % opts.single_fasta_input1_list[single_read_index])
+                    else:
+                        fout.write("b=%s\n" % opts.single_bam_input1_list[single_read_index])
+                    single_read_index = single_read_index + 1
+                elif opts.type_of_data_list[index] == "paired":
+                    if opts.format_of_data_list[index] == "fastq":
+                        fout.write("q1=%s\n" % (opts.paired_fastq_input1_list)[paired_read_index])
+                        fout.write("q2=%s\n" % (opts.paired_fastq_input2_list)[paired_read_index])
+                    elif opts.format_of_data_list[index] == "fasta":
+                        fout.write("f1=%s\n" % opts.paired_fasta_input1_list[paired_read_index])
+                        fout.write("f2=%s\n" % opts.paired_fasta_input2_list[paired_read_index])
+                    else:
+                        fout.write("b1=%s\n" % opts.paired_fasta_input1_list[paired_read_index])
+                        fout.write("b2=%s\n" % opts.paired_fasta_input2_list[paired_read_index])
+                    paired_read_index = paired_read_index + 1
+            fout.close()
+        except Exception, e:
+            stop_err("config file cannot be opened for writing" + str(e))
 
-        #Set up command line call - need to remove hard coded path
-    #    cmd = "/usr/local/bgisoap/soapdenovo/current/bin/SOAPdenovo-63mer all -s %s -o %s" % (tmp_file, tmp_dir + "/result")
-    cmd = "/usr/local/bgisoap/soapdenovo2/bin/SOAPdenovo-63mer all -s %s -K 63 -R -o %s" % (tmp_file,
-                                                                                          tmp_dir + "/result")
+    #Set up command line call - assumes path to executable has been defined in users environment
+    #Need to check kmer size to decide if to use SOAPdenovo-63mer or SOAPdenovo-127mer
+    if opt.kmer_size <= 63 and opts.default_full_settings_type == "default":
+        cmd = "SOAPdenovo-63mer all -s %s -o %s" % (config_file, tmp_dir + "/result")
+    elif opt.kmer_size <= 63 and opts.default_full_settings_type == "full":
+        cmd = "SOAPdenovo-63mer all -s %s -o %s -K %s -p %s -a %s -d %s -R %s -D %s -M %s -m %s -e %s -E %s -k %s -F %s -u %s -w %s -G %s -L %s -c %s -C %s -b %s -B %s -N %s -V %s" % (config_file, tmp_dir + "/result", opts.kmer_size, opts.ncpu, opts.init_mem_assumption, opts.kmer_freq_cutoff, opts.resolve_repeats, opts.edge_cov_cutoff, opts.merge_level, opts.max_k, opts.weight, opts.merge_clean_bubble, opts.kmer_r2c, opts.fill_gaps, opts.unmask_contigs, opts.keep_contigs_connected, opts.gap_len_diff, opts.min_contig_len, opts.min_contig_cvg, opts.max_contig_cvg, opts.insert_size_upper_bound, opts.bubble_coverage, opts.genome_size, opts.ass_visual)
+    elif opt.kmer_size >= 64 and opts.default_full_settings_type == "default":
+        cmd = "SOAPdenovo-127mer all -s %s -o %s" % (config_file, tmp_dir + "/result")
+    else:
+        cmd = "SOAPdenovo-127mer all -s %s -o %s -K %s -p %s -a %s -d %s -R %s -D %s -M %s -m %s -e %s -E %s -k %s -F %s -u %s -w %s -G %s -L %s -c %s -C %s -b %s -B %s -N %s -V %s" % (config_file, tmp_dir + "/result", opts.kmer_size, opts.ncpu, opts.init_mem_assumption, opts.kmer_freq_cutoff, opts.resolve_repeats, opts.edge_cov_cutoff, opts.merge_level, opts.max_k, opts.weight, opts.merge_clean_bubble, opts.kmer_r2c, opts.fill_gaps, opts.unmask_contigs, opts.keep_contigs_connected, opts.gap_len_diff, opts.min_contig_len, opts.min_contig_cvg, opts.max_contig_cvg, opts.insert_size_upper_bound, opts.bubble_coverage, opts.genome_size, opts.ass_visual)
+
     print cmd
 
-    #Perform SOAPdenovo-trans analysis
+    #Perform SOAPdenovo2 analysis
     buffsize = 1048576
     try:
         #Create file in temporary directory
         tmp = tempfile.NamedTemporaryFile(dir=tmp_dir).name
         #Open a stream to file
         tmp_stderr = open(tmp, 'wb')
-        #Call SOAPdenovo-trans
+        #Call SOAPdenovo
         proc = subprocess.Popen(args=cmd, shell=True, cwd=tmp_dir, stderr=tmp_stderr.fileno())
         returncode = proc.wait()
         #Close stream
@@ -164,7 +173,7 @@ def main():
         if returncode != 0:
             raise Exception, stderr
     except Exception, e:
-        raise Exception, 'Problem performing de novo assembly ' + str(e)
+        raise Exception, 'Problem performing SOAPdenovo process ' + str(e)
 
     #Read SOAPdenovo-trans results into outputs
     contig_out = open(opts.contig, 'wb')
@@ -183,10 +192,11 @@ def main():
 
     #Clean up temp files
     cleanup_before_exit(tmp_dir)
+
     #Check results in output file
     if os.path.getsize(opts.contig) > 0:
-        sys.stdout.write('Status complete')
+        sys.stdout.write('SOAPdenovo process finished')
     else:
-        stop_err("The output is empty")
+        stop_err("Problem with SOAPdenovo process")
 
 if __name__ == "__main__": main()
